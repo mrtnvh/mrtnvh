db87055c79b4816601a9ac4eaf4f78e583840c16
Maarten Van Hoof <maartenvanhoof1989@gmail.com>
2019-11-02 23:26:50 +0100

    style(Gridsome): Cleaned up main.js

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
-// This is the main.js file. Import global CSS and scripts here.
-// The Client API can be used here. Learn more: gridsome.org/docs/client-api
+/* eslint-disable no-param-reassign */
 import VueElementQuery from "vue-element-query";
 import VuePortal from "portal-vue";
-
 import DefaultLayout from "./layouts/Default.vue";
 import "normalize.css";
 import "./assets/styles/app.css";
 
-export default function(Vue /* , { router, head, isClient } */) {
-	// Set default layout as a global component
+export default function(Vue) {
+	/**
+	 * Layout
+	 */
 	Vue.component("Layout", DefaultLayout);
+
+	/**
+	 * Plugins
+	 */
 	Vue.use(VueElementQuery);
 	Vue.use(VuePortal);
 }
