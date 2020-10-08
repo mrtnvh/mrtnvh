4dc4e7700bb0782cb8938e6952fb2741565f2499
Maarten Van Hoof <maarten.vanhoof@isaac.nl>
2020-10-09 00:01:51 +0200

    WIP - re-add lowercase titled layout

---
+++ src/layouts/default.vue
@@ -0,0 +1,66 @@
+<template>
+	<div class="layout">
+		<app-header />
+		<main v-show="!portalContent">
+			<Nuxt />
+		</main>
+		<app-footer v-show="!portalContent" />
+		<PortalTarget name="root" class="portal" @change="handlePortalUpdate" />
+	</div>
+</template>
+
+<script>
+import Seo from "~/lib/Seo";
+import pkg from "../../package.json";
+import Header from "~/components/App/Header.vue";
+import Footer from "~/components/App/Footer.vue";
+
+export default {
+	components: {
+		AppHeader: Header,
+		AppFooter: Footer,
+	},
+	data() {
+		return {
+			portalContent: false,
+		};
+	},
+	methods: {
+		handlePortalUpdate(newContent) {
+			this.portalContent = newContent;
+		},
+	},
+	head() {
+		return Seo({ siteUrl: pkg.homepage });
+	},
+};
+</script>
+
+<style>
+.layout {
+	width: 100%;
+	min-height: 100vh;
+	display: flex;
+	flex-direction: column;
+}
+
+main {
+	display: flex !important;
+	flex-direction: column;
+}
+
+main,
+.portal {
+	flex-grow: 1;
+}
+
+.portal {
+	display: flex;
+	flex-direction: column;
+	overflow: hidden;
+
+	&:empty {
+		display: none;
+	}
+}
+</style>
