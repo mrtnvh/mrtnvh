f781d46a7ce733e51767205e9f798a072d6aae4f
Maarten Van Hoof <maartenvanhoof1989@gmail.com>
2021-06-18 07:22:24 +0200

    test(jest): fix jest-expect-message

--- tests/setup/setup-files-after-env.js
+++ tests/setup/setup-files-after-env.js
@@ -1,4 +1,5 @@
-require("jest-expect-message");
 const { toMatchImageSnapshot } = require("jest-image-snapshot");
+const withMessage = require("./jest-expect-message");
 
+global.expect = withMessage(global.expect);
 expect.extend({ toMatchImageSnapshot });
