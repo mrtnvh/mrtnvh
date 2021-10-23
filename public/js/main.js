ccc1ceadfde2812a989622c440b31040249d28e1
Maarten Van Hoof <maartenvanhoof1989@gmail.com>
2021-10-23 15:22:26 +0200

    feat(swup): add page transitions

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,26 @@
+import Swup from "swup";
+import SwupPreloadPlugin from "@swup/preload-plugin";
 import { setImagesSizes } from "./setImagesSizes";
 
-setImagesSizes();
+function init() {
+	setImagesSizes();
+}
+
+const swup = new Swup({
+	plugins: [new SwupPreloadPlugin()],
+});
+
+window.plausible =
+	window.plausible ||
+	// eslint-disable-next-line func-names
+	function () {
+		// eslint-disable-next-line prefer-rest-params
+		(window.plausible.q = window.plausible.q || []).push(arguments);
+	};
+
+swup.on("contentReplaced", init);
+swup.on("pageView", () => {
+	window.plausible("pageview");
+});
+
+init();
