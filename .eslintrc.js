b32abfd634c65ae1b31e3b637c3fc77d53388052
Maarten Van Hoof <maartenvanhoof1989@gmail.com>
2021-12-28 14:55:51 +0100

    test(eslint): disable import/no-named-default

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
 	rules: {
 		"no-console": process.env.NODE_ENV === "production" ? "error" : "off",
 		"no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+		"import/no-named-default": 0,
 		"import/no-unresolved": [
 			"error",
 			{
