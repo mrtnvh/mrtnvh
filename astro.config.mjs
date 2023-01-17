a9e3fd623d26a9ec45ea55df1a1e64e86e0014d0
Maarten Van Hoof <info@mrtnvh.com>
2023-01-17 21:19:47 +0100

    fix(events): cleanup astro config to make dsv work better

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config'; // eslint-disable-next-line import/no-unresolved
 import mdx from '@astrojs/mdx'; // eslint-disable-next-line import/no-unresolved
 import sitemap from '@astrojs/sitemap';
-import { camelCase } from 'lodash-es';
-import { svgSprite } from 'rollup-plugin-svgsprite-generator';
-import copy from 'rollup-plugin-copy';
 import rehypeRewrite from 'rehype-rewrite';
 import dsv from '@rollup/plugin-dsv';
 
@@ -31,51 +28,12 @@ const rehypePlugins = [
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    sitemap(),
-    mdx({
-      rehypePlugins,
-    }),
-  ],
+  integrations: [sitemap(), mdx()],
   markdown: {
     rehypePlugins,
   },
   vite: {
-    plugins: [
-      dsv(),
-      copy({
-        targets: [
-          {
-            src: [
-              './node_modules/@fontsource/ibm-plex-mono/files/ibm-plex-mono-latin-500-normal.woff2',
-              './node_modules/@fontsource/ibm-plex-mono/files/ibm-plex-mono-latin-500-normal.woff',
-              './node_modules/@fontsource/inter/files/inter-latin-400-normal.woff2',
-              './node_modules/@fontsource/inter/files/inter-latin-400-normal.woff',
-              './node_modules/@fontsource/inter/files/inter-latin-500-normal.woff2',
-              './node_modules/@fontsource/inter/files/inter-latin-500-normal.woff',
-              './node_modules/@fontsource/inter/files/inter-latin-700-normal.woff2',
-              './node_modules/@fontsource/inter/files/inter-latin-700-normal.woff',
-              './node_modules/@fontsource/inter/files/inter-latin-800-normal.woff2',
-              './node_modules/@fontsource/inter/files/inter-latin-800-normal.woff',
-            ],
-            dest: 'public/fonts',
-          },
-        ],
-      }),
-    ],
-    packageOptions: {
-      rollup: {
-        plugins: [
-          svgSprite({
-            input: 'public/images',
-            output: 'src/components/SvgSprite.astro',
-            xml: false,
-            doctype: false,
-            idConvert: camelCase,
-          }),
-        ],
-      },
-    },
+    plugins: [dsv()],
   },
   site: 'https://mrtnvh.com',
 });
