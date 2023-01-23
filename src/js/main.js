5a0e8e927168dad05913a1c2c78cdc8223720778
Maarten Van Hoof <info@mrtnvh.com>
2023-01-23 18:15:32 +0100

    fix: Disable Swup for now

--- src/js/main.js
+++ src/js/main.js
@@ -1,59 +1,59 @@
-import Swup from 'swup';
-import SwupHeadPlugin from '@swup/head-plugin';
-import SwupA11yPlugin from '@swup/a11y-plugin';
-import SwupScrollPlugin from '@swup/scroll-plugin';
+// import Swup from 'swup';
+// import SwupHeadPlugin from '@swup/head-plugin';
+// import SwupA11yPlugin from '@swup/a11y-plugin';
+// import SwupScrollPlugin from '@swup/scroll-plugin';
 
-function setBrandSize() {
-  document.querySelectorAll('.brand').forEach(($brand) => {
-    if (window.location.pathname === '/') {
-      $brand.classList.add('brand-large');
-    } else {
-      $brand.classList.remove('brand-large');
-    }
-  });
-}
+// function setBrandSize() {
+//   document.querySelectorAll('.brand').forEach(($brand) => {
+//     if (window.location.pathname === '/') {
+//       $brand.classList.add('brand-large');
+//     } else {
+//       $brand.classList.remove('brand-large');
+//     }
+//   });
+// }
 
-function setNavigationActiveLink() {
-  document.querySelectorAll('#nav a').forEach((link) => {
-    const currentPath = window.location.pathname;
-    const linkPath = new URL(link.href).pathname;
+// function setNavigationActiveLink() {
+//   document.querySelectorAll('#nav a').forEach((link) => {
+//     const currentPath = window.location.pathname;
+//     const linkPath = new URL(link.href).pathname;
 
-    if ((currentPath === '/' || currentPath.includes('topics')) && linkPath === '/') {
-      link.setAttribute('data-page-current', '');
-    } else if (currentPath.includes(linkPath) && linkPath !== '/') {
-      link.setAttribute('data-page-current', '');
-    } else {
-      link.removeAttribute('data-page-current');
-    }
-  });
-}
+//     if ((currentPath === '/' || currentPath.includes('topics')) && linkPath === '/') {
+//       link.setAttribute('data-page-current', '');
+//     } else if (currentPath.includes(linkPath) && linkPath !== '/') {
+//       link.setAttribute('data-page-current', '');
+//     } else {
+//       link.removeAttribute('data-page-current');
+//     }
+//   });
+// }
 
-function onContentReplaced() {
-  setBrandSize();
-  setNavigationActiveLink();
-}
+// function onContentReplaced() {
+//   setBrandSize();
+//   setNavigationActiveLink();
+// }
 
-const swup = new Swup({
-  plugins: [
-    // new SwupPreloadPlugin(),
-    new SwupHeadPlugin({ persistAssets: true }),
-    new SwupA11yPlugin(),
-    new SwupScrollPlugin({ doScrollingRightAway: true, animateScroll: false }),
-  ],
-});
+// const swup = new Swup({
+//   plugins: [
+//     // new SwupPreloadPlugin(),
+//     new SwupHeadPlugin({ persistAssets: true }),
+//     new SwupA11yPlugin(),
+//     new SwupScrollPlugin({ doScrollingRightAway: true, animateScroll: false }),
+//   ],
+// });
 
-window.plausible =
-  window.plausible ||
-  // eslint-disable-next-line func-names
-  function () {
-    // eslint-disable-next-line prefer-rest-params
-    (window.plausible.q = window.plausible.q || []).push(arguments);
-  };
+// window.plausible =
+//   window.plausible ||
+//   // eslint-disable-next-line func-names
+//   function () {
+//     // eslint-disable-next-line prefer-rest-params
+//     (window.plausible.q = window.plausible.q || []).push(arguments);
+//   };
 
-swup.on('contentReplaced', onContentReplaced);
+// swup.on('contentReplaced', onContentReplaced);
 
-swup.on('pageView', () => {
-  window.plausible('pageview');
-});
+// swup.on('pageView', () => {
+//   window.plausible('pageview');
+// });
 
-onContentReplaced();
+// onContentReplaced();
