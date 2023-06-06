2408fcca90d7d0d60b82c2a8400deee52308af7a
Maarten Van Hoof <info@mrtnvh.com>
2023-06-06 07:53:32 +0200

    feat: add view-transition-api (#429)

--- src/js/main.js
+++ src/js/main.js
@@ -1,63 +1,6 @@
-// import Swup from 'swup';
-// import SwupHeadPlugin from '@swup/head-plugin';
-// import SwupA11yPlugin from '@swup/a11y-plugin';
-// import SwupScrollPlugin from '@swup/scroll-plugin';
-
-// function setBrandSize() {
-//   document.querySelectorAll('.brand').forEach(($brand) => {
-//     if (window.location.pathname === '/') {
-//       $brand.classList.add('brand-large');
-//     } else {
-//       $brand.classList.remove('brand-large');
-//     }
-//   });
-// }
-
-// function setNavigationActiveLink() {
-//   document.querySelectorAll('#nav a').forEach((link) => {
-//     const currentPath = window.location.pathname;
-//     const linkPath = new URL(link.href).pathname;
-
-//     if ((currentPath === '/' || currentPath.includes('topics')) && linkPath === '/') {
-//       link.setAttribute('data-page-current', '');
-//     } else if (currentPath.includes(linkPath) && linkPath !== '/') {
-//       link.setAttribute('data-page-current', '');
-//     } else {
-//       link.removeAttribute('data-page-current');
-//     }
-//   });
-// }
-
-// function onContentReplaced() {
-//   setBrandSize();
-//   setNavigationActiveLink();
-// }
-
-// const swup = new Swup({
-//   plugins: [
-//     // new SwupPreloadPlugin(),
-//     new SwupHeadPlugin({ persistAssets: true }),
-//     new SwupA11yPlugin(),
-//     new SwupScrollPlugin({ doScrollingRightAway: true, animateScroll: false }),
-//   ],
-// });
-
-// window.plausible =
-//   window.plausible ||
-//   // eslint-disable-next-line func-names
-//   function () {
-//     // eslint-disable-next-line prefer-rest-params
-//     (window.plausible.q = window.plausible.q || []).push(arguments);
-//   };
-
-// swup.on('contentReplaced', onContentReplaced);
-
-// swup.on('pageView', () => {
-//   window.plausible('pageview');
-// });
-
-// onContentReplaced();
-
+/**
+ * Load HTML templates on click
+ */
 document.documentElement.addEventListener('click', (event) => {
   if (event.target.matches('[data-template-id]')) {
     event.preventDefault();
@@ -67,3 +10,23 @@ document.documentElement.addEventListener('click', (event) => {
     event.target.parentNode.replaceChild(templateContent, event.target);
   }
 });
+
+/**
+ * Preload internal links
+ */
+function handleInternalLinkIntersect(entries) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) return;
+    const newPreLoadLink = document.createElement('link');
+    newPreLoadLink.rel = 'prerender';
+    newPreLoadLink.href = entry.target.href;
+    document.head.appendChild(newPreLoadLink);
+  });
+}
+
+const internalLinkObserver = new IntersectionObserver(handleInternalLinkIntersect);
+[...document.querySelectorAll('a')]
+  .filter(({ href }) => href.includes(window.location.origin) && !href.includes('#') && href !== window.location.href)
+  .forEach((link) => {
+    internalLinkObserver.observe(link);
+  });
