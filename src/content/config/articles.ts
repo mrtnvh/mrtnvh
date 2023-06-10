36b699f5b994ba9d124f0442f08ca461e03c0620
Maarten Van Hoof <info@mrtnvh.com>
2023-06-10 16:03:48 +0200

    feat(articles): add dateEdited

--- src/content/config/articles.ts
+++ src/content/config/articles.ts
@@ -3,6 +3,7 @@ import { defineCollection, z } from 'astro:content';
 export const articlesCollection = defineCollection({
   schema: z.object({
     datePublished: z.number(),
+    dateEdited: z.number().optional(),
     title: z.string(),
     description: z.string(),
     url: z.string().optional(),
