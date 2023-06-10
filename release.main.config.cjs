e4d42a84d3ccf230ceb70319e4584a184aa239bb
Maarten Van Hoof <info@mrtnvh.com>
2023-06-10 23:09:39 +0200

    ci(release: add semantic release and prerelease

---
+++ release.main.config.cjs
@@ -0,0 +1,48 @@
+// const baseConfig = require('./release.config.cjs');
+
+module.exports = {
+  branches: ['main'],
+  dryRun: true,
+  plugins: [
+    // https://github.com/semantic-release/release-notes-generator
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+        presetConfig: {
+          types: [
+            { type: 'feat', section: 'Features' },
+            { type: 'fix', section: 'Bug Fixes' },
+            { type: 'chore', section: 'Chores', hidden: false },
+            { type: 'docs', section: 'Documentation', hidden: false },
+            { type: 'style', section: 'Styles', hidden: false },
+            { type: 'refactor', section: 'Refactors', hidden: false },
+            { type: 'perf', section: 'Performance', hidden: false },
+            { type: 'test', section: 'Tests', hidden: false },
+          ],
+        },
+      },
+    ],
+    // https://github.com/semantic-release/changelog
+    // Must be called before npm and git plugins
+    // Uses the result of the release-notes-generator to generate the CHANGELOG.md.
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+        changelogTitle:
+          '# Changelog\n\nAll notable changes to this project will be documented in this file. See\n[Conventional Commits](https://conventionalcommits.org) for commit guidelines.',
+      },
+    ],
+    // https://github.com/semantic-release/git
+    [
+      '@semantic-release/git',
+      {
+        message: 'chore(release): v<%= nextRelease.version %>',
+        assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
+      },
+    ],
+    // https://github.com/semantic-release/github
+    '@semantic-release/github',
+  ],
+};
